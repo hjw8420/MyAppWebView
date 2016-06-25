bc66d20d147b5485654c299db8c64d2c131b6077
HAN JI WON <hjw8420@gmail.com>
2016-06-25 14:55:02 +0900

    네이버 웹 뷰

---
+++ app/src/main/java/com/example/user/myappwebview/IntroActivity.java
@@ -0,0 +1,36 @@
+package com.example.user.myappwebview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends Activity implements View.OnClickListener { // alt+insert 에서 아래 onClick 메소드 자동생성
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        ((Button) findViewById(R.id.btWebView)).setOnClickListener(this);
+        ((Button) findViewById(R.id.btImageView)).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btWebView:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.btImageView:
+
+                break;
+
+
+        }
+    }
+}
